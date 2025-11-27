f2cf2dfc12b7f6287e78f8461603aeaa7b6c674f
Stephan Rayner <stephan.rayner@gmail.com>
2025-11-27 16:50:35 -0700

    Extending program to french

---
+++ language.py
@@ -0,0 +1,12 @@
+TEXT = {
+    "en": {
+        "request_template": "Please spell, {}",
+        "correct": "Yes, that is correct!",
+        "incorrect": "No, that is not quite correct"
+    },
+    "fr": {
+        "request_template": "Please spell, {}",
+        "correct": "Oui, c'est exact!",
+        "incorrect": "Non, ce n'est pas tout à fait exact."
+    }
+}
--- main.py
+++ main.py
@@ -3,11 +3,15 @@
 import time
 import random
 import subprocess
+from language import TEXT
+
+# WORDS, LANGUAGE = ["hill", "puff", "toss", "buzz", "fizz", "smell", "sniff", "grass", "press", "still"], "en"
+WORDS, LANGUAGE = ["fraise", "balai", "vrai", "jamais", "lait", "beige", "neige", "pleine", "reine", "seize"], "fr"
 
 def say(message, language="en"):
     # print(message, language)
     if language == "fr":
-        cmd = ["say", "--voice", "Thomas"]
+        cmd = ["say", "--voice", "Amélie (Premium)"]
     elif language == "en":
         cmd = ["say"]
     else:
@@ -16,23 +20,24 @@ def say(message, language="en"):
     
     result = subprocess.run([*cmd, f"{message}"], capture_output=True, text=True)
     return result.stdout
+
+def main(words: list[str], messages: dict[str, str]):
     
+    words = [w.lower() for w in WORDS]
 
+    for word in WORDS:
+        request =  messages["request_template"].format(word)
+        say(request, LANGUAGE)
 
-words = ["hill", "puff", "toss", "buzz", "fizz", "smell", "sniff", "grass", "press", "still"]
-# words = ["fraise", "balai", "vrai", "jamais", "lait", "beige", "neige", "pleine", "reine", "seize"]
+        result = input("Type answer here: ").lower()
+        if result == word:
+            say(messages["correct"], LANGUAGE)
+        else:
+            say(messages["incorrect"], LANGUAGE)
 
+        time.sleep(1)
 
-random.shuffle(words)
-
-for word in words:
-    msg = f"Please spell {word}"
-    say(msg, "en")
-
-    result = input("Type answer here: ")
-    if result == word:
-        say("That is correct!")
-    else:
-        say("That is not quite correct")
-    
-    time.sleep(1)
+if __name__ == "__main__":
+    random.shuffle(WORDS)
+    messages = TEXT[LANGUAGE]
+    main(WORDS, messages)
